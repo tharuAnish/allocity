9a137647edf5387faa5230afe9e594a04a2359ec
tharuAnish <namoanishtharu@gmail.com>
2022-12-08 18:40:02 +0530

    data fetched , styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      <h1>AlloCity</h1>
+    <div>
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect, useCallback } from "react"
+import CountryCard from "../../components/CountryCard"
+import "./home.style.css"
+
+export default function Home() {
+  const [countryData, setCountryData] = useState([])
+  const [url, setUrl] = useState("https://restcountries.com/v2/all")
+  const [isLoading, setIsLodaing] = useState(false)
+
+  const fetchCountry = useCallback(async () => {
+    const res = await fetch(url)
+    const json = await res.json()
+    setCountryData(json)
+    console.log(countryData)
+  }, [url])
+
+  useEffect(() => {
+    fetchCountry()
+  }, [url, fetchCountry])
+
+  return (
+    <div>
+      <div className="card-wrapper wrapper">
+        {countryData.length ? (
+          countryData.map((item, i) => (
+            <CountryCard key={i} data={item} isLoading={isLoading} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  )
+}
